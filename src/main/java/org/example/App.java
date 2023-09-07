5060561816c90451d833f29b97b23fcac1cc4061
Pavel Radkevich <pradkevich@eisgroup.com>
2023-09-07 17:29:18 +0300

    added some fields

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,15 +2,17 @@ package org.example;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
+public class App {
+
+    private static int x;
+    private static boolean myBool;
+
+    public static void main(String[] args) {
+
+        int x;
+
+        //System.out.println(x);
+        System.out.println(myBool);
     }
 }
